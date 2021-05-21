3c32409c0c68af13875442bcb5268cb3d52f3690
Marco Alabruzzo <marco.alabruzzo@gmail.com>
2021-05-21 16:54:57 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/tic-tac-toe.ts
+++ src/main/tic-tac-toe.ts
@@ -15,6 +15,22 @@ export enum Move {
     lr
 }
 
+const horizontalWins = [
+    [Move.tl, Move.tm, Move.tr],
+    [Move.ml, Move.mm, Move.mr],
+    [Move.ll, Move.lm, Move.lr],
+]
+const verticalWins = [
+    [Move.tl, Move.ml, Move.ll],
+    [Move.tm, Move.mm, Move.lm],
+    [Move.tr, Move.mr, Move.lr],
+]
+const diagonalWins = [
+    [Move.tl, Move.mm, Move.lr],
+    [Move.tr, Move.mm, Move.ll],
+]
+
+const allWins = [...horizontalWins, ...verticalWins, ...diagonalWins]
 
 export class Game {
     currentPlayer: Player = Player.X
@@ -22,29 +38,33 @@ export class Game {
     board: Player[]|null[] = [null, null, null ,null, null, null, null ,null, null];
 
     move(input: Move) {
-        this.validateMove(this.currentPlayer)
-        console.log(this.board)
         if (this.board[input] !== null) {
+            // console.log(this.board)
             return false
         }
         this.board[input] = this.currentPlayer
+        if (this.checkWin()) {
+            return `${this.currentPlayer} won the game!`
+        }
+        this.swapPlayers()
+        // console.log(this.board)
         return true
     }
 
+    checkWin() {
+        for (let index = 0; index < allWins.length; index++) {
+            const winningCombination = allWins[index];
+            const pos1 = this.board[winningCombination[0]]
+            const pos2 = this.board[winningCombination[1]]
+            const pos3 = this.board[winningCombination[2]]
+            if ((this.currentPlayer === pos1) && (pos1 === pos2) && (pos2 === pos3)) {
+                return true
+            }
+        }
+    }
 
-    validateMove(input: Player) {
-        if (input === Player.X && this.currentPlayer === null) {
-            this.currentPlayer = input;
-            return true;
-        }
-        if (input === Player.O && this.currentPlayer === null) {
-            return false;
-        }
-        if (input !== this.currentPlayer) {
-            this.currentPlayer = input;
-            return true;
-        }
-        return false;
+    swapPlayers() {
+        this.currentPlayer = this.currentPlayer === Player.X ? Player.O : Player.X
     }
 }
 
--- src/test/tic-tac-toe.test.ts
+++ src/test/tic-tac-toe.test.ts
@@ -1,33 +1,42 @@
 import {Game, Move, Player} from "../main/tic-tac-toe";
 
-describe('First turn', () => {
-    it("'X' is a valid first turn", () => {
-        const game = new Game();
-        expect(game.validateMove(Player.X)).toBe(true);
+// describe('First turn', () => {
+//     it("'X' is a valid first turn", () => {
+//         const game = new Game();
+//         expect(game.validateMove(Player.X)).toBe(true);
 
-    })
-    it("'O' is not valid first turn", () => {
-        const game = new Game();
-        expect(game.validateMove(Player.O)).toBe(false);
-    })
-    it("'X-0-X-O' is a valid sequence", () => {
-        const game = new Game();
-        expect(game.validateMove(Player.X)).toBe(true);
-        expect(game.validateMove(Player.O)).toBe(true);
-        expect(game.validateMove(Player.X)).toBe(true);
-        expect(game.validateMove(Player.O)).toBe(true);
-    })
-    it("A repetition is not a valid turn", () => {
-        const game = new Game();
-        expect(game.validateMove(Player.X)).toBe(true);
-        expect(game.validateMove(Player.X)).toBe(false);
-    })
-})
+//     })
+//     it("'O' is not valid first turn", () => {
+//         const game = new Game();
+//         expect(game.validateMove(Player.O)).toBe(false);
+//     })
+//     it("'X-0-X-O' is a valid sequence", () => {
+//         const game = new Game();
+//         expect(game.validateMove(Player.X)).toBe(true);
+//         expect(game.validateMove(Player.O)).toBe(true);
+//         expect(game.validateMove(Player.X)).toBe(true);
+//         expect(game.validateMove(Player.O)).toBe(true);
+//     })
+//     it("A repetition is not a valid turn", () => {
+//         const game = new Game();
+//         expect(game.validateMove(Player.X)).toBe(true);
+//         expect(game.validateMove(Player.X)).toBe(false);
+//     })
+// })
 
 describe("Move test", () => {
     it("A player can only move to an unoccupied space", () => {
         const game = new Game();
         expect(game.move(Move.tl)).toBe(true);
         expect(game.move(Move.tl)).toBe(false);
+        expect(game.move(Move.ll)).toBe(true);
     })
+    it("3 X in a row win the game", () => {
+         const game = new Game();
+         game.move(Move.tl)
+         game.move(Move.ll)
+         game.move(Move.tm)
+         game.move(Move.lm)
+         expect(game.move(Move.tr)).toBe('X won the game!')
+     })
 })
